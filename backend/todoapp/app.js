7b39491dd9ea6125168b7ed1d6829bc72fcfb953
Siddhardha Tagirisa <siddhardha.t@setuserv.com>
2018-11-08 19:48:22 +0530

    FIX: Warning.

--- backend/todoapp/app.js
+++ backend/todoapp/app.js
@@ -14,7 +14,7 @@ var app = express();
 
 var mongoose = require('mongoose')
 mongoose.Promise = bluebird
-mongoose.connect('mongodb://127.0.0.1:27017/todoapp', { useMongoClient: true})
+mongoose.connect('mongodb://127.0.0.1:27017/todoapp', { useNewUrlParser: true })
 .then(()=> { console.log(`Succesfully Connected to the Mongodb Database  at URL : mongodb://127.0.0.1:27017/todoapp`)})
 .catch(()=> { console.log(`Error Connecting to the Mongodb Database at URL : mongodb://127.0.0.1:27017/todoapp`)})
 
